a5d8794fba9f9c2a3a19cbadb43da6213f5f8031
Xavier Stamps-Lafont <xavierstampslafont@gmail.com>
2023-05-29 22:18:38 -0400

    Code formatter updates

--- public/main.js
+++ public/main.js
@@ -1,94 +1,94 @@
-const VCard = window.vcardcreator;
+const VCard = window.vcardcreator
 
-const form = document.getElementById("form");
+const form = document.getElementById("form")
 
 const initInput = (key) => {
-  const input = document.getElementById(key);
+  const input = document.getElementById(key)
 
-  const checkboxKey = `${key}-included`;
+  const checkboxKey = `${key}-included`
 
-  const checkbox = document.getElementById(checkboxKey);
+  const checkbox = document.getElementById(checkboxKey)
 
-  input.value = localStorage.getItem(key);
+  input.value = localStorage.getItem(key)
 
-  checkbox.checked = localStorage.getItem(checkboxKey) === "true";
+  checkbox.checked = localStorage.getItem(checkboxKey) === "true"
 
   input.addEventListener("input", (event) => {
-    const value = event.target.value;
+    const value = event.target.value
 
-    localStorage.setItem(key, value);
+    localStorage.setItem(key, value)
 
-    const booleanValue = Boolean(value);
+    const booleanValue = Boolean(value)
 
-    checkbox.checked = booleanValue;
+    checkbox.checked = booleanValue
 
-    localStorage.setItem(checkboxKey, booleanValue);
+    localStorage.setItem(checkboxKey, booleanValue)
 
-    updateQR();
-  });
+    updateQR()
+  })
 
   checkbox.addEventListener("change", () => {
-    localStorage.setItem(checkboxKey, checkbox.checked);
+    localStorage.setItem(checkboxKey, checkbox.checked)
 
-    updateQR();
-  });
+    updateQR()
+  })
 
-  return [input, checkbox];
-};
+  return [input, checkbox]
+}
 
-const [firstNameInput, firstNameIncluded] = initInput("first-name");
-const [lastNameInput, lastNameIncluded] = initInput("last-name");
-const [phoneInput, phoneIncluded] = initInput("phone");
-const [emailInput, emailIncluded] = initInput("email");
+const [firstNameInput, firstNameIncluded] = initInput("first-name")
+const [lastNameInput, lastNameIncluded] = initInput("last-name")
+const [phoneInput, phoneIncluded] = initInput("phone")
+const [emailInput, emailIncluded] = initInput("email")
 
-const qrCodeElement = document.getElementById("qrCode");
-const qrCode = new QRCode(qrCodeElement);
+const qrCodeElement = document.getElementById("qrCode")
+const qrCode = new QRCode(qrCodeElement)
 
-const qrPlaceholder = document.getElementById("qrPlaceholder");
+const qrPlaceholder = document.getElementById("qrPlaceholder")
 
 form.onsubmit = (event) => {
-  event.preventDefault();
+  event.preventDefault()
 
-  updateQR();
-};
+  updateQR()
+}
 
 const updateQR = () => {
-  const vCard = new VCard();
+  const vCard = new VCard()
 
   const { hasLastName, hasFirstName, hasPhone, hasEmail, hasAnyInfo } =
-    getInfoState();
+    getInfoState()
 
   vCard.addName(
     hasLastName ? lastNameInput.value : "",
     hasFirstName ? firstNameInput.value : ""
-  );
+  )
 
   if (hasPhone) {
-    vCard.addPhoneNumber(phoneInput.value);
+    vCard.addPhoneNumber(phoneInput.value)
   }
 
   if (hasEmail) {
-    vCard.addEmail(emailInput.value);
+    vCard.addEmail(emailInput.value)
   }
 
   if (hasAnyInfo) {
-    qrCode.makeCode(vCard.toString());
-    qrCodeElement.style.visibility = "visible";
-    qrPlaceholder.style.visibility = "hidden";
+    qrCode.makeCode(vCard.toString())
+    qrCodeElement.style.visibility = "visible"
+    qrPlaceholder.style.visibility = "hidden"
   } else {
-    qrCodeElement.style.visibility = "hidden";
-    qrPlaceholder.style.visibility = "visible";
+    qrCodeElement.style.visibility = "hidden"
+    qrPlaceholder.style.visibility = "visible"
   }
-};
+}
 
 const getInfoState = () => {
-  const hasLastName = lastNameIncluded.checked && Boolean(lastNameInput.value);
+  const hasLastName = lastNameIncluded.checked && Boolean(lastNameInput.value)
   const hasFirstName =
-    firstNameIncluded.checked && Boolean(firstNameInput.value);
-  const hasPhone = phoneIncluded.checked && Boolean(phoneInput.value);
-  const hasEmail = emailIncluded.checked && Boolean(emailInput.value);
+    firstNameIncluded.checked && Boolean(firstNameInput.value)
+  const hasPhone = phoneIncluded.checked && Boolean(phoneInput.value)
+  const hasEmail = emailIncluded.checked && Boolean(emailInput.value)
 
-  const hasAnyInfo = hasLastName || hasFirstName || hasPhone || hasEmail;
+  const hasAnyInfo = hasLastName || hasFirstName || hasPhone || hasEmail
 
   return {
     hasLastName,
@@ -96,11 +96,11 @@ const getInfoState = () => {
     hasPhone,
     hasEmail,
     hasAnyInfo,
-  };
-};
+  }
+}
 
-const { hasAnyInfo } = getInfoState();
+const { hasAnyInfo } = getInfoState()
 
 if (hasAnyInfo) {
-  updateQR();
+  updateQR()
 }
